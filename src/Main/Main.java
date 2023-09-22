b9b658f5c0961f4b217955d0f3df73fc6974c360
frajavimopu <frajavimopu@gmail.com>
2023-09-22 16:29:03 -0500

    First commit

---
+++ src/Main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * ITESS-TICS
+ * Taller de investigación 1
+ * Intento de Curso de certificación java 0Z1-808
+ * By FJMP
+ * 22/septiembre/2023
+ */
+package Main;
+
+/**
+ *
+ * @author TICS03
+ */
+public class Main {
+    public static void main(String ...arg) {
+       // TODO
+    }
+}
